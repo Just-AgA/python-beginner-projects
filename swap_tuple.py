ca03b77f0ad53dd4431063959541fae1ef34625f
AgA <161239277+Just-AgA@users.noreply.github.com>
2024-12-10 19:43:33 +0100

    simple tuple unpacking

---
+++ swap_tuple.py
@@ -0,0 +1,9 @@
+#Swap the elements of two tuples
+tuple1 = (11, 22)
+tuple2 = (99, 88)
+
+#Pack and unpack the elements of the tuples in the following way
+tuple1, tuple2 = tuple2, tuple1
+
+print(tuple1)
+print(tuple2)
