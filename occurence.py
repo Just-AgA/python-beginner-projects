6ba7144c199ba2478b0c7f09360d4aa35cc72729
AgA <161239277+Just-AgA@users.noreply.github.com>
2024-12-17 19:00:56 +0100

    find the count of a specific string regardless of casing

---
+++ occurence.py
@@ -0,0 +1,5 @@
+#Find the occurence of a string in a sentence
+str1 = "Welcome to USA. usa awesome, isn't it?"
+
+#Lowercase the entire string,and then use the .count() method
+print(str1.lower().count("usa"))
